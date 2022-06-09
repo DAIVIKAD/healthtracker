53630f8a89c45d6e44e9b344a010052d7cea9a23
DAIVIKAD <106665016+DAIVIKAD@users.noreply.github.com>
2022-06-09 10:32:40 +0530

    Initial commit

---
+++ htapp/models.py
@@ -0,0 +1,86 @@
+import re
+from sre_parse import Verbose
+from telnetlib import STATUS
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+# Create your models here.
+
+
+class AppUserManager(BaseUserManager):
+
+    def create_superuser(self, email, branch_code, staff_id, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        return self.create_user (email, branch_code, staff_id, password, **other_fields)
+
+    def create_user( self, email, branch_code, staff_id, password, **other_fields):
+        if not email:
+            raise ValueError('You must provide a valid email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, branch_code = branch_code, staff_id = staff_id, ** other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=80, null=True, verbose_name = "First Name")
+    last_name = models.CharField(max_length=80, null=True, verbose_name = "Last Name")
+    branch_code = models.CharField(max_length=20, null=True, verbose_name = "Branch Code")
+    staff_id =  models.CharField(max_length=20, null=True, verbose_name = "Staff Id")
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    start_date = models.DateTimeField(default=timezone.now)
+
+    objects = AppUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['branch_code', 'staff_id']
+
+    def __str__(self):
+        return self.staff_id
+
+       
+
+class Person(models.Model):
+
+    Gender = (('M','Male'),
+        ('F','Female'),
+        ('M','Male'),
+        ('O','Other'))
+
+    Ratings = (('1', '*'),
+        ('2','**'),
+        ('3','***'),
+        ('4','****'),
+        ('5','*****'))
+
+    Status = (('DEAD','Dead'),
+        ('SICK','sick'),
+        ('HEALTHY','Healthy'),
+        ('UNHEALTHY','Unhealthy'))
+
+
+    Person_name = models.CharField(max_length=100,blank=True, null=True, verbose_name="Person name")
+    Person_address = models.CharField(max_length=120, blank=True, null=True, verbose_name="Person address")
+    Person_pri_phone= models.CharField(max_length=13, blank=True, null=True, verbose_name="Person pri_phone")
+    Person_sec_phone = models.CharField(max_length=13, blank=True, null=True, verbose_name="Person sec_phone")
+    Person_dob = models.DateField(blank=True,null=True,verbose_name="DOB")
+    Person_gender = models.CharField(max_length=1, choices= Gender, default='M', verbose_name="Gender")
+    Person_email = models.EmailField(max_length=100, blank=True, null=True, verbose_name="Email Address")
+    Person_status = models.CharField(max_length=20, blank=True, null=True, verbose_name="health status")
+    Person_notes = models.CharField(max_length=200, blank=True, null=True, verbose_name="health notes")
+    Person_rating = models.CharField(max_length=1, choices=Ratings,default=5,verbose_name="Rating")
+    
+
+    def __str__(self):
+        return self.Person_name
+
+
+        
