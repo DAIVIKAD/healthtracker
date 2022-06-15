c94ca5a3faa2ccaad2da717ecbe4b2818a4c47ea
DAIVIKAD <106665016+DAIVIKAD@users.noreply.github.com>
2022-06-15 09:27:38 +0530

    serilizer update

---
+++ htapp/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework import serializers
+from htapp.models import AppUser, Person , Hospital,  Appointment,  Doctor_detail, Pharmacy, Fees, Invoice, Fees_type, Insurance,Laboratory, Feed_back
